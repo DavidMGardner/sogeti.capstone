ec24f8d684659d72cef8f3ab27ed27f956fb2eb8
Dave Gardner <dave@texasgardners.com>
2015-06-22 09:16:27 -0500

    Initial Project Create

---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sogeti.Capstone.Web.Startup))]
+namespace Sogeti.Capstone.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
