1b2bd27a9c70f9dd987572d1914169f65812b891
TjitseSogeti <tjitse.miller@us.sogeti.com>
2015-06-24 16:00:01 -0500

    Added EventType and Status Integration tests

---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Data.IntegrationTests/EventTypeDataIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+using Sogeti.Capstone.Data.Model;
+
+namespace Sogeti.Capstone.Data.IntegrationTests
+{
+    [TestFixture]
+    public class EventTypeDataIntegrationTests
+    {
+        private static readonly CapstoneContext Context = new CapstoneContext("Sogeti.Capstone.Data.Model.CapstoneContext");
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            IntegrationTestDatabaseInitalizer.AssemblyInit(Context);
+        }
+
+        [Test]
+        public void Add_EventType_With_Defaults()
+        {
+            //arrange
+            var newEventType = new EventType()
+            {
+                Id = 1
+            };
+
+            //act
+            Context.EventType.Add(newEventType);
+            Context.SaveChanges();
+
+            //assert
+            int rowCount = Context.EventType.Count();
+            rowCount.ShouldBeGreaterThan(0);
+        }
+    }
+}
