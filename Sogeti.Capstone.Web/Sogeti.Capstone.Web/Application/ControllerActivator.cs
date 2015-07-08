6ae022f3004481e6f35be3f86cafc68ede37dab0
Dave Gardner <dave@texasgardners.com>
2015-07-08 09:41:54 -0500

    added the 2 missing files

---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Web/Application/ControllerActivator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ShortBus;
+using StructureMap;
+
+namespace Sogeti.Capstone.Web.Application
+{
+
+    public class StructureMapControllerFactory : DefaultControllerFactory
+    {
+        private readonly IContainer _container;
+
+        public StructureMapControllerFactory(IContainer container)
+        {
+            _container = container;
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == null)
+                return null;
+
+            var container = _container.GetNestedContainer();
+            Debug.WriteLine(container.WhatDoIHave());
+
+            var controller = (IController)container.GetInstance(controllerType);
+            
+            return controller;
+        }
+    }
+}
---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Web/Application/Mediator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+using ShortBus;
+using StructureMap;
+
+namespace Sogeti.Capstone.Web.Application
+{
+    public interface IMediator
+    {
+        Task<TResponseData> RequestAsync<TResponseData>(IAsyncRequest<TResponseData> query);
+        Task NotifyAsync<TNotification>(TNotification notification);
+    }
+
+    public class Mediator : IMediator
+    {
+        readonly IContainer _container;
+
+        public Mediator(IContainer container)
+        {
+            _container = container;
+        }
+
+        public Task<TResponseData> RequestAsync<TResponseData>(IAsyncRequest<TResponseData> query)
+        {
+            var plan = new MediatorPlan<TResponseData>(typeof(IAsyncRequestHandler<,>), "HandleAsync", query.GetType(), _container);
+
+            return plan.InvokeAsync(query);
+        }
+
+        public Task NotifyAsync<TNotification>(TNotification notification)
+        {
+            var handlers = _container.GetAllInstances<IAsyncNotificationHandler<TNotification>>();
+
+            return Task.WhenAll(handlers.Select(x => x.HandleAsync(notification)));
+        }
+
+        class MediatorPlan<TResult>
+        {
+            readonly MethodInfo HandleMethod;
+            readonly Func<object> HandlerInstanceBuilder;
+
+            public MediatorPlan(Type handlerTypeTemplate, string handlerMethodName, Type messageType, IContainer container)
+            {
+                var handlerType = handlerTypeTemplate.MakeGenericType(messageType, typeof(TResult));
+
+                HandleMethod = GetHandlerMethod(handlerType, handlerMethodName, messageType);
+
+                HandlerInstanceBuilder = () => container.GetInstance(handlerType);
+            }
+
+            MethodInfo GetHandlerMethod(Type handlerType, string handlerMethodName, Type messageType)
+            {
+                return handlerType
+                    .GetMethod(handlerMethodName,
+                        BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod,
+                        null, CallingConventions.HasThis,
+                        new[] { messageType },
+                        null);
+            }
+
+            public Task<TResult> InvokeAsync(object message)
+            {
+                return (Task<TResult>)HandleMethod.Invoke(HandlerInstanceBuilder(), new[] { message });
+            }
+        }
+    }
+}
