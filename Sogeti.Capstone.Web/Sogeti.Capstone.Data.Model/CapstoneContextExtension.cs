7a456894481c278ea218895cfc432ad6e29a2adb
Dave Gardner <dave@texasgardners.com>
2015-08-05 10:47:56 -0500

    Adding missing project

---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Data.Model/CapstoneContextExtension.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Entity;
+
+namespace Sogeti.Capstone.Data.Model
+{
+    public static class CapstoneContextExtension
+    {
+        public static void DeleteAllDbSetData(this CapstoneContext context)
+        {
+            var dataDeleter = new DatabaseDataDeleter(context);
+
+            RemoveDbSetData(context.Events);
+            RemoveDbSetData(context.EventType);
+            RemoveDbSetData(context.Category);
+            RemoveDbSetData(context.Registrations);
+            RemoveDbSetData(context.Statuses);
+            context.SaveChanges();
+
+            dataDeleter.DeleteAllObjects();
+        }
+
+        public static void DeleteDataFromDbSet(this CapstoneContext context, DbSet set)
+        {
+            var dataDeleter = new DatabaseDataDeleter(context);
+
+            RemoveDbSetData(set);
+            context.SaveChanges();
+
+            dataDeleter.DeleteAllObjects();
+        }
+
+        private static void RemoveDbSetData(DbSet set)
+        {
+            foreach (var @entity in set)
+            {
+                set.Remove(@entity);
+            }
+        }
+    }
+}
