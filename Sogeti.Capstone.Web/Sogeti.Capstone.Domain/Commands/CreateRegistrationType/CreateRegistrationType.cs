88d5493e639eb0882171cab05d37a8a82ad5e058
Ryan Wishnow <ryan.wishnow@gmail.com>
2015-08-10 16:13:43 -0500

    Added CreateRegistrationType Command

---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Domain/Commands/CreateRegistrationType/CreateRegistrationType.cs
@@ -0,0 +1,31 @@
+﻿using ShortBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sogeti.Capstone.Domain.Commands.CreateRegistrationType
+{
+    public class CreateRegistrationType : IAsyncRequest<CreateRegistrationTypeResult>
+    {
+        public string Title { get; set; }
+
+        public CreateRegistrationType()
+        {
+
+        }
+
+        public CreateRegistrationType(string title)
+        {
+            Title = title;
+        }
+    }
+
+    public class CreateRegistrationTypeResult
+    {
+        public int Id { get; set; }
+    }
+
+
+}
---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Domain/Commands/CreateRegistrationType/CreateRegistrationTypeHandler.cs
@@ -0,0 +1,27 @@
+﻿using ShortBus;
+using Sogeti.Capstone.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moo.Extenders;
+
+namespace Sogeti.Capstone.Domain.Commands.CreateRegistrationType
+{
+    public class CreateRegistrationTypeHandler : IAsyncRequestHandler<CreateRegistrationType, CreateRegistrationTypeResult>
+    {
+        private static readonly CapstoneContext Context =
+            new Sogeti.Capstone.Data.Model.CapstoneContext("Sogeti.Capstone.Data.Model.CapstoneContext");
+
+        public async Task<CreateRegistrationTypeResult> HandleAsync(CreateRegistrationType request)
+        {
+            var dataModel = request.MapTo<Sogeti.Capstone.Data.Model.RegistrationType>();
+            var response = Context.Category.Add(dataModel);
+
+            await Context.SaveChangesAsync();
+
+            return new CreateRegistrationTypeResult { Id = response.Id };
+        }
+    }
+}
