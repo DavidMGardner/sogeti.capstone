0882b297022ddcdcd841b41fe06d84a3698c8749
Ryan Wishnow <ryan.wishnow@gmail.com>
2015-08-18 10:39:04 -0500

    Finished writing/fixing last integration test

---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Domain/Commands/CreateRegistration/CreateRegistration.cs
@@ -0,0 +1,31 @@
+﻿using ShortBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sogeti.Capstone.Domain.Commands.CreateRegistration
+{
+    public class CreateRegistration : IAsyncRequest<CreateRegistrationResult>
+    {
+        public string Title { get; set; }
+        public DateTime RegisterDateTime { get; set; }
+
+        public CreateRegistration()
+        {
+
+        }
+
+        public CreateRegistration(string title, DateTime register)
+        {
+            Title = title;
+            RegisterDateTime = register;
+        }
+    }
+
+    public class CreateRegistrationResult
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Sogeti.Capstone.Web/Sogeti.Capstone.Domain/Commands/CreateRegistration/CreateRegistrationHandler.cs
@@ -0,0 +1,28 @@
+﻿using ShortBus;
+using Sogeti.Capstone.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moo.Extenders;
+
+namespace Sogeti.Capstone.Domain.Commands.CreateRegistration
+{
+    public class CreateRegistrationHandler : IAsyncRequestHandler<CreateRegistration, CreateRegistrationResult>
+    {
+        private static readonly CapstoneContext Context =
+             new Sogeti.Capstone.Data.Model.CapstoneContext("Sogeti.Capstone.Data.Model.CapstoneContext");
+
+        public async Task<CreateRegistrationResult> HandleAsync(CreateRegistration request)
+        {
+            var dataModel = request.MapTo<Sogeti.Capstone.Data.Model.Registration>();
+            var response = Context.Registrations.Add(dataModel);
+
+            await Context.SaveChangesAsync();
+
+            return new CreateRegistrationResult { Id = response.Id };
+        }
+        
+    }
+}
